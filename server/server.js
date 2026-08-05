01de89b76c8b2410c92c11b6b1dccc1705bcca6d
Mayank Vaishnav <mayankvaishnav687@gmail.com>
2026-08-05 18:27:27 +0530

    Fix CORS for production

--- server/server.js
+++ server/server.js
@@ -33,7 +33,10 @@ app.use(
 );
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://vedamapnextech-crm.onrender.com",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
